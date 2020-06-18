52ae91bba3913c275cd3519668a5593986768a60
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-18 15:18:36 -0500

    Add self relationships to asana model

--- app/models/asana.rb
+++ app/models/asana.rb
@@ -5,6 +5,13 @@ class Asana < ApplicationRecord
   has_many :categories, through: :asanacategories
   has_many :opener_asanas, foreign_key: :opener_id, class_name: 'OpeningAsana'
   has_many :openees, through: :opener_asanas
+  has_many :counter_asanas, foreign_key: :counter_id, class_name: 'CounterAsana'
+  has_many :countees, through: :counter_asanas
+  has_many :stabalizer_asanas, foreign_key: :stabalizerr_id, class_name: 'StabalizingAsana'
+  has_many :stabalizee, through: :stabalizer_asanas
+  has_many :preparer_asanas, foreign_key: :preparer_id, class_name: 'PreparationAsana'
+  has_many :preparees, through: :preparer_asanas
+
 
   def self.createasana(asanarray)
     self.create({:sanskritname => asanarray[0],
