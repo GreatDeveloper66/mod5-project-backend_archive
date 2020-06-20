40bd1dfae4359a9cca0a262a2552e6489b35183f
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-19 20:16:35 -0500

    Add stabalizingasanas migration

---
+++ db/migrate/20200620010824_create_stabalizingasanas.rb
@@ -0,0 +1,8 @@
+class CreateStabalizingasanas < ActiveRecord::Migration[6.0]
+  def change
+    create_table :stabalizingasanas do |t|
+      t.integer :stabalizer_id
+      t.integer :stabalizee_id
+    end
+  end
+end
