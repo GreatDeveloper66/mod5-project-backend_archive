74de71ac2a61201b2cea2d188796e73d4330b898
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-19 20:16:21 -0500

    Add preparationasanas migration

---
+++ db/migrate/20200620005951_create_preparationasanas.rb
@@ -0,0 +1,8 @@
+class CreatePreparationasanas < ActiveRecord::Migration[6.0]
+  def change
+    create_table :preparationasanas do |t|
+      t.integer :preparer_id
+      t.integer :preparee_id
+    end
+  end
+end
