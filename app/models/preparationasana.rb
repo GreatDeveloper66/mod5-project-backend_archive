e1e02fcd8c75c01f4bcff9e9bc851c90ae25506b
GreatDeveloper66 <ashaffe@gmail.com>
2020-06-23 12:06:52 -0500

    Add foreign keys to preparation model

--- app/models/preparationasana.rb
+++ app/models/preparationasana.rb
@@ -1,4 +1,4 @@
 class PreparationAsana < ApplicationRecord
-  belongs_to :preparer, class_name: 'Asana'
-  belongs_to :preparee, class_name: 'Asana'
+  belongs_to :preparer, foreign_key: 'preparer_id', class_name: 'Asana'
+  belongs_to :preparee, foreign_key: 'preparee_id', class_name: 'Asana'
 end
