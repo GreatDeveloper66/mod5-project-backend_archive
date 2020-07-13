8f771ad25f25d9f2ff697d7818ec8ad6799dcf20
GreatDeveloper66 <ashaffe@gmail.com>
2020-07-13 00:37:58 -0500

    Add createpreparers method to asana.rb

--- app/models/asana.rb
+++ app/models/asana.rb
@@ -22,9 +22,26 @@ class Asana < ApplicationRecord
   end
   def createcounters(counter_asnas)
     counter_asanas.each do |counter_asana|
-      opener_id = counter_asana.id
-      openee_id = self.id
-      CounterAsana.create({"counter_id => opener_id, :countee_id => openee_id})
+      counter_id = counter_asana.id
+      countee_id = self.id
+      CounterAsana.create({counter_id => counter_id, :countee_id => countee_id})
     end
   end
+
+  def createopeners(opener_asnas)
+    opener_asanas.each do |opener_asana|
+      opener_id = opener_asana.id
+      openee_id = self.id
+      OpeningAsana.create({opener_id => opener_id, :openee_id => openee_id})
+    end
+  end
+
+  def createpreparers(preparer_asnas)
+    preparer_asanas.each do |preparer_asana|
+      preparer_id = preparer_asana.id
+      preparee_id = self.id
+      PrearationAsana.create({preparer_id => preparer_id, :preparee_id => preparee_id})
+    end
+  end
+
 end
